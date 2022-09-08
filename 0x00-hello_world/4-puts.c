45164c651946723b18cc8596d7289d54ff0c0bf2
Nelsino1 <samuelnelsino@gmail.com>
2022-09-08 16:02:47 -0700

    programming puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * Main - Entry point
  *
  * Return: 0 after printing the function 
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

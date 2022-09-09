efe95f92d7e23f11abe681c414cbed419c9b77ed
Nelsino1 <samuelnelsino@gmail.com>
2022-09-08 17:59:44 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
-*main - print quote
+* main - print quote
 *
 * Description: prints Dora's quote
 *
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-write (2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write (2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

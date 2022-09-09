b7271fee940e7b833a6d335cbf6d2c1e11f8b1c2
Nelsino1 <samuelnelsino@gmail.com>
2022-09-09 07:36:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,13 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
+ * - Dora Korpar, 2015-10-19", followed by a new line, to the standard error.
  * Return: 0 if successful Otherwise 1
  */
 
 int main(void)
-
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 
 }

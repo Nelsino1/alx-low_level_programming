7e593551758d72f1fc95f92b18a0b1599c61c0f7
Nelsino1 <samuelnelsino@gmail.com>
2022-09-15 04:48:27 -0700

    remaining

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -19,7 +20,7 @@ day++;
 }
 
 printf("Day of the year: %d\n", day);
-printf("Ramining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
@@ -33,4 +34,4 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-}
+i}

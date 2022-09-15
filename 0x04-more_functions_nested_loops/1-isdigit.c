54323e32080915e93a2947173edc6001d132df7b
Nelsino1 <samuelnelsino@gmail.com>
2022-09-15 05:50:39 -0700

    is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,11 +5,12 @@
  * @c: interger inputted
  * Return: 1 if digit or 0 otherwise
  */
+
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if ((c >= 48 && (c <= 57))
 	{
-	return (1);
+		return (1);
 	}
 	return (0);
 }

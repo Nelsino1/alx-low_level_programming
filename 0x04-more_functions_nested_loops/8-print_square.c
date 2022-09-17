6f322ba7cbf7f5cbef674c5127fea1875ce20a9b
Nelsino1 <samuelnelsino@gmail.com>
2022-09-17 14:11:08 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-*print-square - prints squares
-*@size: parameter
-*Return: return nothing
-*/
+ * print_square - prints squares
+ * @size: parameter
+ * Return: returns nothing
+ */
 
 void print_square(int size)
 {

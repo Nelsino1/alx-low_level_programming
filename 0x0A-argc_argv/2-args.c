4e9351113ef4543b34f0d6aa55c04e52f91585bc
Nelsino1 <samuelnelsino@gmail.com>
2022-09-30 04:47:13 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,11 +4,11 @@
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
- * @argv[]: array of arguments
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
@@ -16,4 +16,6 @@ int main (int argc, char *argv[])
 	{
 		printf("%s\n", argv[i]);
 	}
+
+	return (0);
 }
